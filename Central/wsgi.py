c9a3907898682371e2233c6b724444404b40c339
Nowa <dainerdual@gmail.com>
2025-10-23 21:30:31 -0400

    Remover creación automática de superusuario

--- Central/wsgi.py
+++ Central/wsgi.py
@@ -14,15 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Central.settings')
 
 
-# 🚀 Bloque temporal para crear superusuario en Render
-if os.environ.get("CREATE_SUPERUSER") == "True":
-    import django
-    django.setup()
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser("admin", "admin@example.com", "admin123")
-        print("✅ Superusuario 'admin' creado con éxito.")
-
 application = get_wsgi_application()
