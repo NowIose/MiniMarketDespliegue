1503471c7cb81d002e112eca1aca3713c857a1d2
Gabriel Choque <gabrielchoque222@gmail.com>
2025-11-05 02:13:51 -0400

    Suministro Mejorado

--- Inventario/urls.py
+++ Inventario/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('proveedores/', views.lista_proveedores, name='listar_proveedores'),
     path('proveedores/agregar/',views.agregar_proveedor, name='agregar_proveedor'),
     path('proveedores/editar/<int:pk>', views.editar_proveedor, name= 'editar_proveedor'),
+    path('suministros/nuevo/', views.nuevo_suministro, name='nuevo_suministro'),
+    path('suministros/', views.lista_suministros, name='lista_suministros'),
 ]
